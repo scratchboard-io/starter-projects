678731a7817ebd1e22fcab9158d69808e15694c0
Collins Abitekaniza <abtcolns@gmail.com>
2023-11-21 11:50:43 +0300

    initial commit

---
+++ arduino/sketch.ino
@@ -0,0 +1,14 @@
+byte leds[] = {2, 3, 4};
+void setup() {
+  for (byte i = 0; i < sizeof(leds); i++) {
+    pinMode(leds[i], OUTPUT);
+  }
+}
+
+int i = 0;
+void loop() {
+  digitalWrite(leds[i], HIGH);
+  delay(250);
+  digitalWrite(leds[i], LOW);
+  i = (i + 1) % sizeof(leds);
+}
---
+++ blank_digital_circuit/README.md
@@ -0,0 +1,3 @@
+# Blank Digital Circuit
+
+<p>There's nothing to see here ¯\_(ツ)_/¯</p>
